f85ab65ed526dff94ebad7c02974b1acc575e1fb
Muetzilla <muetz-github@gasbichler.ch>
2022-01-04 11:24:36 +0100

    new method

--- m411-gruppenarbeit-magnus-loic-dennis-moritz/src/main/java/dev/modul411/sortiergruppenarbeit/Main.java
+++ m411-gruppenarbeit-magnus-loic-dennis-moritz/src/main/java/dev/modul411/sortiergruppenarbeit/Main.java
@@ -5,41 +5,27 @@ import dev.modul411.sortiergruppenarbeit.importfile.ReadFile;
 /**
  * Main Class
  *
- * @author Dennis Woodtli, Loic Flückiger, Magnus Götz, Moritz Gasbichler
+ * @author Magnus Götz, Moritz Gasbichler
  * @version 1.0
  * @since 2022-01-04
  */
 public class Main {
     private final String pathPrefix = "src/main/resources/importfiels/";
+    private int[] tenDigits;
+    private int[] hundredDigits;
+    private int[] thousandDigits;
 
     public Main() {
-        getImputs();
-    }
-
-    public void getImputs() {
         ReadFile readFile = new ReadFile();
-        int[] tenDigits;
-        int[] hundredDigits;
-        int[] thousandDigits;
-
         tenDigits = readFile.readLinesFromFile(pathPrefix + "10Digits.dat");
-        System.out.println("The Digits of the 10 Digits File: ");
-        printDigits(tenDigits);
-
         hundredDigits = readFile.readLinesFromFile(pathPrefix + "100Digits.dat");
-        System.out.println("The Digits of the 100 Digits File: ");
-        printDigits(hundredDigits);
-
         thousandDigits = readFile.readLinesFromFile(pathPrefix + "1000Digits.dat");
-        System.out.println("The Digits of the 1000 Digits File: ");
-        printDigits(thousandDigits);
     }
 
-    private void printDigits(int[] digits) {
-        for (int i : digits) {
-            System.out.println(i);
-        }
+    public void compareSortingAlgorithm() {
+        //TODO sort and messaure the files using the three arrays read from the files
     }
+
 
     public static void main(String[] args) {
         Main main = new Main();

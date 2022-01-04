cd0474ac92ef390996e83ddf91e4226454e52c4b
Muetzilla <muetz-github@gasbichler.ch>
2022-01-04 08:35:27 +0100

    Main

---
+++ src/main/java/dev/modul411/sortiergruppenarbeit/Main.java
@@ -0,0 +1,8 @@
+package dev.modul411.sortiergruppenarbeit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is the Mainfile for our Gruppenarbeit.");
+    }
+}
